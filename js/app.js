6b717e10b8b06831e26cfe79aac2a98c88d672a0
Daniel Coats <d0coat01@louisville.edu>
2018-05-15 18:15:35 -0700

    Removed header, revamped showcase section, added skills section with Vue implementation

--- js/app.js
+++ js/app.js
@@ -9,3 +9,116 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+var skills = new Vue({
+  el: '#skills',
+  data: {
+    skills: [
+      {
+        name: "Javascript",
+        icon: "fab fa-js",
+        experience: "6 Years",
+        tools: [
+          {
+            name: "ES6",
+            experience: "6 months",
+            description: "",
+          },
+          {
+            name: "AngularJS 1.x",
+            experience: "4 years",
+            description: "",
+          },
+          {
+            name: "React",
+            experience: "1 month",
+            description: "",
+          },
+          {
+            name: "Gulp",
+            experience: "3 years",
+            description: "",
+          },
+        ]
+      },
+      {
+        name: "Web Design",
+        icon: "fab fa-html5",
+        experience: "6 Years",
+        tools: [
+          {
+            name: "Responsive Web Design",
+            experience: "5 years",
+            description: "",
+          },
+          {
+            name: "Bootstrap",
+            experience: "4 years",
+            description: "",
+          },
+          {
+            name: "CSS Preprocessing (Less)",
+            experience: "3 years",
+            description: "",
+          },
+        ]
+      },
+      {
+        name: "PHP",
+        icon: "fab fa-php",
+        experience: "5 Years",
+        tools: [
+          {
+            name: "CakePHP",
+            experience: "1.5 years",
+            description: "",
+          },
+          {
+            name: "PHPUnit",
+            experience: "6 months",
+            description: "",
+          },
+          {
+            name: "AWS SDK",
+            experience: "6 months",
+            description: "",
+          },
+        ]
+      },
+      {
+        name: "Relational Databases",
+        icon: "fas fa-database",
+        experience: "5 Years",
+        tools: [
+          {
+            name: "MySQL",
+            experience: "4 years",
+            description: "",
+          },
+          {
+            name: "PostgreSQL",
+            experience: "6 months",
+            description: "",
+          },
+        ]
+      },
+      {
+        name: "Python",
+        icon: "fab fa-python",
+        experience: "6 months",
+        tools: [
+          {
+            name: "Flask",
+            experience: "3 months",
+            description: "",
+          },
+          {
+            name: "SqlAlchemy",
+            experience: "2 months",
+            description: "",
+          },
+        ]
+      },
+    ]
+  }
+})
---
+++ sw.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/main.css',
+  '/css/responsive.css'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
